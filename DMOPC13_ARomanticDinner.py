c7f714344a02aafd96d16167579fd8ce585f4991
P234rex <david.dave.xie@gmail.com>
2017-09-17 23:40:33 -0400

    Update DMOPC13_ARomanticDinner.py

--- DMOPC13_ARomanticDinner.py
+++ DMOPC13_ARomanticDinner.py
@@ -5,12 +5,12 @@
 
 m,u,r = map(int,raw_input().split())
 
-dp = [[0 for i in xrange(u+1)] for j in xrange(m+1)]                        #initialize 2D dp array
+dp = [[0 for i in xrange(u+1)] for j in xrange(m+1)]                        #Initialize 2D dp array
 
 for x in xrange(r):
     val,t,f = map(int,raw_input().split())
     for i in xrange(m, t-1, -1):
-        for j in xrange(u, f-1, -1):                                        #iterate backwards to avoid overlap
+        for j in xrange(u, f-1, -1):                                        #Iterate backwards to avoid overlap
             dp[i][j] = max(dp[i][j], val+dp[i-t][j-f])
 
 print dp[m][u]
